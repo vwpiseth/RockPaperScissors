a5327bc69e06ecc3c9ded9c1585046fb920f80f3
vuthvatanakpiseth99@gmail.com <113506120+vwpiseth@users.noreply.github.com>
2023-09-12 22:19:22 +0700

    Adding ui

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,84 +1,150 @@
-const elementArr = ['Rock','Paper','Scissors'];
 
-function getComputerChoice(){
+document.addEventListener('DOMContentLoaded', function() {
+    const elementArr = ['Rock','Paper','Scissors'];
+    const para = document.querySelector('.roundResult');
+    const computerChoice  = document.querySelector('.computerChoice');
+    const userChoice = document.querySelector('.userChoice');
 
-    //random the array to pick an element 
-    const randomIndex = Math.floor(Math.random()* elementArr.length);
-    console.log("Computer Selection: " + elementArr[randomIndex]);
-    return elementArr[randomIndex];
 
-}
+    function getComputerChoice(){
+
+        //random the array to pick an element 
+        const randomIndex = Math.floor(Math.random()* elementArr.length);
+        // console.log("Computer Selection: " + elementArr[randomIndex]);
+        computerChoice.textContent = `The Computer Pick : ${elementArr[randomIndex].toUpperCase()}`;
+        return elementArr[randomIndex];
+    
+    }
+    
 
 //initiate variables to count points for both players
- let userPoint = 0, computerPoint = 0;
+   let userPoint = 0, computerPoint = 0;
 
-function playRound(playerSelection, computerSelection){
+    function playRound(playerSelection, computerSelection){
 
-    if(playerSelection === "rock" && computerSelection === "scissors"){ 
-        userPoint++;
-        return console.log("You Win!, Rock beats Scissors");
-    }
-    else if(playerSelection === "rock" && computerSelection === "paper"){
-        computerPoint++;
-        return console.log("You lose, Paper beats Rock");
-    }
-    else if(playerSelection === "scissors" && computerSelection === "rock"){
-        computerPoint++;
-        return console.log("You Lose!, Rock beats Scissors");
-    }
-    else if(playerSelection === "scissors" && computerSelection === "paper"){
-        userPoint++;
-        return console.log("You Win!, Scissors beats Rock");
-    }
-    else if(playerSelection === "paper" && computerSelection === "scissors"){
-        computerPoint++;
-        return console.log("You Lose!, Scissors beats Paper");
-    }
-    else if(playerSelection === "paper" && computerSelection === "rock"){
-        userPoint++;
-        return console.log("You Win!, Paper beats Rock");
-    }
-    else{ 
-        return console.log("It's a draw");
-    }
-   
-}
-
-
-function pointChecking(){
-
-    if(userPoint > computerPoint){
-        return console.log("Congratulation, YOU WIN!");
-    }else if(userPoint < computerPoint){
-        return console.log("You lose :( ");
-    }
-    else return console.log("It's a draw");
-}
-
-function game(){
-    //loops 5 times to play 5 rounds of games
-    for (let i =0; i<5; i++){
+        if(playerSelection === "rock" && computerSelection === "scissors"){ 
+            userPoint++;
+             para.textContent = "You Win!, ROCK beats SCISSORS";
+        }
+        else if(playerSelection === "rock" && computerSelection === "paper"){
+            computerPoint++;
+             para.textContent = "You Lose!, PAPER beats ROCK" ;
+        }
+        else if(playerSelection === "scissors" && computerSelection === "rock"){
+            computerPoint++;
+             para.textContent = "You Lose!, ROCK beats SCISSORS";
+        }
+        else if(playerSelection === "scissors" && computerSelection === "paper"){
+            userPoint++;
+             para.textContent = "You Win!, SCISSORS beats PAPER";
+        }
+        else if(playerSelection === "paper" && computerSelection === "scissors"){
+            computerPoint++;
+             para.textContent = "You Lose!, SCISSORS beats PAPER" ;
+        }
+        else if(playerSelection === "paper" && computerSelection === "rock"){
+            userPoint++;
+             para.textContent = "You Win!, PAPER beats ROCK" ;
+        }
+        else{ 
+             para.textContent = "It's a draw" ;
+        }
     
-        console.log("Round " + parseInt(i+1));
-
-         //Make sure playerSelection parameter are case-insensitive
-        let playerSelection = prompt("Rock Paper or Scissors : ").toLowerCase();
-
-        console.log("Your Selection: " + playerSelection);
-
-        //Make computerSelection to Lower Case too to make sure it matches with playerSelection format in the playRound Function
-        const computerSelection = getComputerChoice().toLowerCase();
-
-        playRound(playerSelection,computerSelection);  
     }
 
+    const rockBtn = document.querySelector('.btnRock');
+    const paperBtn = document.querySelector('.btnPaper');
+    const scissorsBtn = document.querySelector('.btnScissors');
+    const userScore = document.querySelector('.userPoint');
+    const computerScore = document.querySelector('.computerPoint');
+    const result = document.querySelector('.result');
+    const roundNumber = document.querySelector('.roundNumber');
+    const startMessage = document.querySelector('h2');
+    const restartBtn = document.querySelector('.restartButton');
+    let round =0;
+    
+    function pointChecking(){
+        if(round===5){
+            if(userPoint > computerPoint){
+            result.textContent = "Congratulation, YOU WIN!";
+        }else if(userPoint < computerPoint){
+            result.textContent = "Sorry, You Lose :( ";
+        }
+        else result.textContent = "It's a Draw !";
+         while (restartBtn.firstChild) {
+            restartBtn.removeChild(restartBtn.firstChild);
+        }
+        const button = document.createElement("button");
+        button.textContent = textContent = "Play Again";
+        button.style.cssText = "background-color:turquoise; color: white; width:300px; height: 50px; font-size: 20px; boarder-radius: 10px; margin-top : 10px;" 
+        button.onclick = function(){
+            location.reload();
+        }
+        restartBtn.appendChild(button);
+        }
+       
+    }
 
-    console.log("RESULT: ");
-    console.log("Your Point :" + userPoint);
-    console.log("Computer Point :" +computerPoint);
-    pointChecking();
-}
-game();
+    rockBtn.addEventListener('click', function () {
+        if(round<5){
+            startMessage.textContent = "";
+            roundNumber.textContent = `Round ${parseInt(round+1)}`;
+            const playerSelection = "rock";
+            const computerSelection = getComputerChoice().toLowerCase();
+            userChoice.textContent = `Your Pick : ${playerSelection.toUpperCase()}`;
+            playRound(playerSelection,computerSelection);
+            userScore.textContent = `Your current point : ${userPoint}`;
+            computerScore.textContent = `Computer current point ${computerPoint}`;
+            round++ ;
+           pointChecking();
+        
+        } 
+        
+    });
+
+    paperBtn.addEventListener('click', function (){
+        if(round<5){
+            startMessage.textContent = "";
+            roundNumber.textContent = `Round ${parseInt(round+1)}`;
+            const playerSelection = "paper";
+            const computerSelection = getComputerChoice().toLowerCase();
+            userChoice.textContent = `Your Pick : ${playerSelection.toUpperCase()}`;
+            playRound(playerSelection,computerSelection); 
+            userScore.textContent = `Your current point : ${userPoint}`;
+            computerScore.textContent = `Computer current point ${computerPoint}`;
+            round++;
+           pointChecking(); 
+        }
+    });
+
+    scissorsBtn.addEventListener('click', function (){
+        if(round<5){
+            startMessage.textContent = "";
+            roundNumber.textContent = `Round ${parseInt(round+1)}`;
+            const playerSelection = "scissors";
+            const computerSelection = getComputerChoice().toLowerCase();
+            userChoice.textContent = `Your Pick : ${playerSelection.toUpperCase()}`;
+            playRound(playerSelection,computerSelection);  
+            userScore.textContent = `Your current point : ${userPoint}`;
+            computerScore.textContent = `Computer current point ${computerPoint}`;
+            round++;
+           pointChecking(); 
+        }
+    });
+});
+
+
+
+
+
+
+
+//     console.log("RESULT: ");
+//     console.log("Your Point :" + userPoint);
+//     console.log("Computer Point :" +computerPoint);
+//     pointChecking();
+
+
 
 
 
